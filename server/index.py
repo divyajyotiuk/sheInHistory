8f8e1995a0e3c996fdb3f6e2bb53befbfd6a68fd
Divyajyoti Ukirde <Lenovo@MacBook-Air-229.local>
2023-09-17 06:15:40 -0400

    ocsp

--- server/index.py
+++ server/index.py
@@ -13,5 +13,5 @@ def locationInfoApi():
     res = db.getEntry(parsed_data['state'])
     print(res)
     return res
-if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True,port=5000)

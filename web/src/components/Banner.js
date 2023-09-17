83fcaf059de733f5be55753db1cddafb6c9a344b
Vanshika Mehrotra <54116079+vanshika2809@users.noreply.github.com>
2023-09-17 02:58:14 -0700

    Create Banner.js

---
+++ web/src/components/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Banner.css'; 
+
+function Banner() {
+  return (
+    <div className="banner">
+      <h1>History of Women in the United States</h1>
+      <p>Women: The Architects of Tomorrow</p>
+    </div>
+  );
+}
+
+export default Banner;

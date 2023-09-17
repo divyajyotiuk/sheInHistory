e28d3cd193c5615ab5e3c25fa79e262e2b2a2ce6
Divyajyoti Ukirde <Lenovo@MacBook-Air-229.local>
2023-09-17 05:45:36 -0400

    index

--- server/index.py
+++ server/index.py
@@ -1,16 +1,17 @@
 from flask import Flask, request
 from flask_cors import CORS
 from db import Database
+import json
+
 app = Flask(__name__)
 cors = CORS(app)
 db=Database(app)
 
-
 @app.route('/api/location', methods=["POST"])
 def locationInfoApi():
     print(request.get_data())
-    state = str(request.get_data())
-    res = db.getEntry(state)
+    parsed_data = json.loads(request.get_data())
+    res = db.getEntry(parsed_data['state'])
     return res
 # if __name__ == '__main__':
 #     app.run(debug=True,port=5000)
